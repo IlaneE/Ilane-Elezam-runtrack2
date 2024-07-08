5c31db88fac2c3e51cb50b04b773a540ade80aac
IlaneE <ilane.elezam@laplateforme.io>
2024-07-08 16:28:18 +0200

    modif

---
+++ jour02/job01/index.php
@@ -0,0 +1,28 @@
+<?php
+
+$c = 0 ;
+
+while ($c != 1337) {
+    echo $c . "</br>";
+    $c = $c +1;
+    if ($c == 42) {
+        echo "<strong><u>" .$c. "</strong></u></br>";
+    }
+}
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+http://localhost/runtrack2/jour02/job01/index.php
