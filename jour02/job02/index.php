1168d27ee7ee190a58ac1e4219919dc49d5e1f29
IlaneE <ilane.elezam@laplateforme.io>
2024-07-08 16:46:10 +0200

    modif

--- jour02/job02/index.php
+++ jour02/job02/index.php
@@ -5,12 +5,24 @@ $c = 0 ;
 while ($c != 1337) {
     echo $c . "</br>";
     $c = $c +1;
-    if ($c == 42) {
-        echo "<strong><u>" .$c. "</strong></u></br>";
+    if ($c == 26) {
+        $c = $c + 1;
+    }
+    if ($c == 37) {
+        $c = $c + 1;
+    }
+    if ($c == 88) {
+        $c = $c + 1;
+    }
+    if ($c == 1111) {
+        $c = $c + 1;
+    }
+    if ($c == 3233) {
+        $c = $c + 1;
     }
 }
 
-echo "1337";
+echo "1337"; 
 
 ?>
 <!DOCTYPE html>
@@ -25,4 +37,4 @@ echo "1337";
 </body>
 </html>
 
-http://localhost/runtrack2/jour02/job01/index.php
+http://localhost/runtrack2/jour02/job02/index.php
