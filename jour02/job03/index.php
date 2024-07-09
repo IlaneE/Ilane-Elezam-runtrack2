0657fc9b69e38ce2f9d4e87fec1e811028baff1e
IlaneE <ilane.elezam@laplateforme.io>
2024-07-09 11:08:18 +0200

    modif

---
+++ jour02/job03/index.php
@@ -0,0 +1,38 @@
+<?php
+
+$c = 0 ;
+$c2 = 21 ;
+$c3 = 26 ;
+$c4 = 51 ; 
+
+while ($c != 20) {
+    $c = $c +1;
+    echo "<i>" . $c . "</i></br>";
+}
+while ($c2 != 26) {
+    echo $c2 . "</br>";
+    $c2 = $c2 +1;
+}
+while ($c3 != 51) {
+    echo "<u>" . $c3 . "</u></br>";
+    $c3 = $c3 +1;
+}
+while ($c4 != 101) {
+    echo $c4 . "</br>";
+    $c4 = $c4 +1;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+</br></br></br>
+</body>
+</html>
+
+http://localhost/runtrack2/jour02/job03/index.php
