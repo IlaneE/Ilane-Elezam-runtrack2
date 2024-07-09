463c6a283a349484d6a7706dc1087c41004c3a61
IlaneE <ilane.elezam@laplateforme.io>
2024-07-09 12:17:26 +0200

    ajout

---
+++ jour02/job04/index.php
@@ -0,0 +1,28 @@
+<?php
+
+$c = 1 ;
+$k = 0;
+
+while ($c != 100) {
+    echo $c . "</br>";
+    $c = $c +1;
+    if ($k%3) {
+        echo 'Fizz';
+    }
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+</br></br></br>
+</body>
+</html>
+
+http://localhost/runtrack2/jour02/job04/index.php
