7e096bc7e189e6864e47476e9667d01f6e8065c0
IlaneE <ilane.elezam@laplateforme.io>
2024-07-15 11:58:35 +0200

    ajout

---
+++ jour03/exo/exo1.php
@@ -0,0 +1,45 @@
+
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculette</title>
+</head>
+<body>
+<form method="POST" action="#" >
+<input type="text" name="n1">
+<select name="op" >                       
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+         <option value="/">/</option>
+        </select> 
+<input type="text" name="n2">
+<input type="submit" name="b1" value="Calculer">
+</br></br></br>
+<?php
+$n1=$_POST['n1'] ?? 0;
+	$op=$_POST['op'] ?? "+";
+	$n2=$_POST['n2'] ?? 0;
+		if (isset($op)){
+    echo("Resultat du calcul ".$n1.$op.$n2." : ");
+    if ($op=="+") {
+        echo $n1+$n2;
+    } elseif ($op=="-") {
+        echo $n1-$n2;
+    } elseif ($op=="*") {
+        echo $n1*$n2;
+    } else {
+        echo $n1/$n2;
+    }
+}
+
+?>
+</body>
+</html>
+
+</br></br></br>
+http://localhost/runtrack2/jour03/exo/exo1.php
