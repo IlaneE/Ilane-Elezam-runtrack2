6a5dae377722f42ccc18239586494d80b9bb3d73
IlaneE <ilane.elezam@laplateforme.io>
2024-07-22 10:34:50 +0200

    ajout

---
+++ jour03/job07/index.php
@@ -0,0 +1,27 @@
+<?php
+
+$txt = "Certaines choses changent, et d'autres ne changeront jamais.";
+$c = $txt [0];
+
+
+for($i = 1; $i != 60; $i ++) {
+    echo $txt[$i];
+}
+echo $c;
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+</br></br></br>
+<p class="lien">http://localhost/runtrack2/jour03/job07/index.php</p>
+</body>
+</html>
---
+++ jour03/job07/test.php
@@ -0,0 +1,36 @@
+<?php
+
+$txt = "Certaines choses changent, et d'autres ne changeront jamais.";
+$c = $txt [0];
+/*$txt [0] == [60];
+while ($txt [1] != $txt [59]){
+    echo substr ($txt, 1, 59);
+    if ($txt == [59]){
+        echo $txt [60];
+    }
+}*/
+
+for($i = 1; $i != 60; $i ++) {
+    echo $txt[$i];
+}
+echo $c;
+
+/*
+$txt = "Certaines choses changent, et d'autres ne changeront jamais.";
+echo substr ($txt, 1, 59);
+*/
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+</br></br></br>
+<p class="lien">http://localhost/runtrack2/jour03/job07/test.php</p>
+</body>
+</html>
